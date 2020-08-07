f7e488f2b5b48fce34c83ad251e9ffa89f43bc28
Sean Demske <seandemske@gmail.com>
2020-08-07 18:16:30 -0400

    Updates

---
+++ precache-manifest.1d72c667723d4ea1ef2e964d987bfc61.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f58b86479db492def5a9431a16f641a3",
+    "url": "/redmarkets-dice/index.html"
+  },
+  {
+    "revision": "1e4bca5fc49eaa3edc74",
+    "url": "/redmarkets-dice/static/css/main.66cc5596.chunk.css"
+  },
+  {
+    "revision": "b3b1c68856344ad01d13",
+    "url": "/redmarkets-dice/static/js/2.a89cc2ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/redmarkets-dice/static/js/2.a89cc2ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e4bca5fc49eaa3edc74",
+    "url": "/redmarkets-dice/static/js/main.61abc5c5.chunk.js"
+  },
+  {
+    "revision": "4dfdd30fb0799dea257d",
+    "url": "/redmarkets-dice/static/js/runtime-main.24d6cfb0.js"
+  },
+  {
+    "revision": "442771e872445dcc14ae39f49b89bea7",
+    "url": "/redmarkets-dice/static/media/backgroundimg3.442771e8.png"
+  }
+]);
